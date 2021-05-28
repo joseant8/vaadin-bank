f6d7eed7ca65b21732efe32ffec75bfc417a2a4a
joseant8 <j2a2mari@gmail.com>
2021-05-28 12:24:15 +0200

    mostrar las cuentas en cards en vista cuentas

--- src/main/java/com/ingenia/bank/views/cuenta/CuentasView.java
+++ src/main/java/com/ingenia/bank/views/cuenta/CuentasView.java
@@ -5,16 +5,22 @@ import com.ingenia.bank.backend.model.Usuario;
 import com.ingenia.bank.backend.service.CuentaService;
 import com.ingenia.bank.backend.service.MovimientoService;
 import com.ingenia.bank.backend.service.UsuarioService;
+import com.ingenia.bank.components.CardCuenta;
 import com.ingenia.bank.views.cuenta.form.CuentaDialog;
 import com.ingenia.bank.views.main.MainView;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridVariant;
 import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.data.provider.ListDataProvider;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
+import java.awt.*;
+import java.util.List;
 import java.util.Optional;
 
 @Route(value = "cuentas", layout = MainView.class)
@@ -24,25 +30,29 @@ public class CuentasView extends VerticalLayout {
     private Grid<Cuenta> grid = new Grid<>();
 
     private Optional<Usuario> currentUser;
+    private List<Cuenta> cuentasList;
 
     private CuentaService cuentaService;
     private MovimientoService movimientoService;
+
 
     public CuentasView(CuentaService cuentaService, MovimientoService movimientoService, UsuarioService usuarioService) {
 
         this.movimientoService = movimientoService;
         this.cuentaService = cuentaService;
         this.currentUser = usuarioService.obtenerUsuarioActualConectado();
+        this.cuentasList = cuentaService.obtenerTodasCuentasByUsuarioId(currentUser.get().getId());
 
         setSizeFull();
         createGrid();
 
-        this.grid.setDataProvider(new ListDataProvider<>(cuentaService.obtenerTodasCuentasByUsuarioId(currentUser.get().getId())));
+        this.grid.setDataProvider(new ListDataProvider<>(this.cuentasList));
         this.grid.addItemClickListener(event -> {
+            UI.getCurrent().getSession().setAttribute("idCuenta", event.getItem().getId());
             new CuentaDialog(this.cuentaService, this.movimientoService, event.getItem().getId()).open();
         });
 
-        add(new H2("Cuentas"),grid);
+        add(new H2("Cuentas"), createCardCuentas(), grid);
     }
 
 
@@ -61,5 +71,34 @@ public class CuentasView extends VerticalLayout {
         grid.addThemeVariants(GridVariant.LUMO_NO_BORDER,GridVariant.LUMO_ROW_STRIPES);
 
         return grid;
+    }
+
+    /**
+     * Crea un componente de cards con todas las cuentas del usuario. Los cards se muestran en una misma fila hasta llegar a 3,
+     * por lo que si hay más de 3 cuentas, se crearán varias filas de cards.
+     * @return componente con todas las cuentas en formato card
+     */
+    private VerticalLayout createCardCuentas(){
+
+        VerticalLayout vl = new VerticalLayout();
+        HorizontalLayout hl = new HorizontalLayout();
+        int numCardsPerRow = 3;  // en cada layout horizontal habrá 3 cards como máximo.
+        int contador = 0;
+
+        for (Cuenta cuenta: this.cuentasList){
+            contador++;
+
+            if(contador <= numCardsPerRow){
+                hl.add(new CardCuenta(cuenta, this.cuentaService, this.movimientoService));
+            }else{
+                contador = 1;
+                vl.add(hl);
+                hl = new HorizontalLayout();
+                hl.add(new CardCuenta(cuenta, this.cuentaService, this.movimientoService));
+            }
+        }
+
+        vl.add(hl);
+        return vl;
     }
 }
