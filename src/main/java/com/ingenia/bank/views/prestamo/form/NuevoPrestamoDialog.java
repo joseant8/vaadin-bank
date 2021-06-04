2e09cee3e2cd5df783daeb1bcb029df6b9f1722b
joseant8 <j2a2mari@gmail.com>
2021-06-04 12:27:52 +0200

    cambios en dialog préstamo

--- src/main/java/com/ingenia/bank/views/prestamo/form/NuevoPrestamoDialog.java
+++ src/main/java/com/ingenia/bank/views/prestamo/form/NuevoPrestamoDialog.java
@@ -6,52 +6,67 @@ import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dialog.Dialog;
-import com.vaadin.flow.component.html.H4;
 import com.vaadin.flow.component.html.H5;
-import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
 public class NuevoPrestamoDialog extends Dialog {
 
-    public static enum DIALOG_RESULT {SAVE, CANCEL};
-    private DIALOG_RESULT dialogResult;
+    private boolean isConfirmed = false;
 
     public NuevoPrestamoDialog(Prestamo prestamo){
         super();
+        add(dataPrestamoDialog(prestamo), createToolbarLayout());
+    }
 
-        add(new H5("Cuenta Ingreso"), new H5(prestamo.getCuentaIngreso().getIban()));
+    private Component dataPrestamoDialog(Prestamo prestamo){
 
-        add(new H5("Cuenta Cobro"), new H4(prestamo.getCuentaCobro().getIban()));
+        VerticalLayout vl = new VerticalLayout();
 
-        add(new H5("Cantidad"), new H4(prestamo.getCantidad().toString()));
+        HorizontalLayout hl1 = new HorizontalLayout();
+        hl1.add(new H5("Cuenta ingreso: "), new H5(prestamo.getCuentaIngreso().getIban()));
 
-        add(createToolbarLayout());
+        HorizontalLayout hl2 = new HorizontalLayout();
+        hl2.add(new H5("Cuenta cobro: "), new H5(prestamo.getCuentaCobro().getIban()));
 
+        HorizontalLayout hl3 = new HorizontalLayout();
+        hl3.add(new H5("Cantidad del préstamo: "), new H5(prestamo.getCantidad() + " €"));
+
+        HorizontalLayout hl4 = new HorizontalLayout();
+        hl4.add(new H5("Interés: "), new H5(prestamo.getInteres() + " %"));
+
+        HorizontalLayout hl5 = new HorizontalLayout();
+        hl5.add(new H5("Cuota: "), new H5(prestamo.getCuota() + " €"));
+
+        HorizontalLayout hl6 = new HorizontalLayout();
+        hl6.add(new H5("Número de cuotas: "), new H5(prestamo.getNumeroCuotas().toString()));
+
+        vl.add(hl1, hl2, hl3, hl4, hl5, hl6);
+        return vl;
 
     }
 
-    public boolean dialogResultIsSave(){
-        if(this.dialogResult == DIALOG_RESULT.SAVE){
-            return true;
-        }else{
-            return false;
-        }
+    public boolean dialogResultIsConfirmed(){
+        return isConfirmed;
     }
 
+    /**
+     * Crea los botones del dialog para aceptar (solicitar préstamo) o cancelar la operación.
+     * @return componente
+     */
     private Component createToolbarLayout() {
 
-        Button saveButton = new Button("Confirm", event -> {
-            this.dialogResult = DIALOG_RESULT.SAVE;
-            Notification.show("Préstamo creado", 5000, Notification.Position.TOP_END);
+        Button saveButton = new Button("Solicitar préstamo", event -> {
+            this.isConfirmed = true;
             close();
         });
 
         saveButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         saveButton.addClickShortcut(Key.ENTER).listenOn(this);
-        saveButton.getElement().getStyle().set("margin-left", "auto");
+        saveButton.getElement().getStyle().set("margin-right", "auto");
 
-        Button cancelButton = new Button("Cancel", event -> {
-            this.dialogResult = DIALOG_RESULT.CANCEL;
+        Button cancelButton = new Button("Cancelar", event -> {
+            this.isConfirmed = false;
             close();
         });
 
